d9afc7fa7cd6ac5debbf3c3827bed9908594054d
cnanfack <cnanfack@gmail.com>
2022-07-08 02:20:39 -0400

    added Swagger-ui Configurations

--- src/main/java/com/homeworkgoto/cardsgame/CardsgameApplication.java
+++ src/main/java/com/homeworkgoto/cardsgame/CardsgameApplication.java
@@ -1,13 +1,34 @@
 package com.homeworkgoto.cardsgame;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CardsgameApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardsgameApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI springShopOpenAPI() {
+		return new OpenAPI()
+				.components(new Components().addSecuritySchemes("basicScheme",
+						new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+				.info(new Info().title("Game Deck (shoe) API")
+						.description("Homework Goto - Charly")
+						.version("v0.0.1")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org"))
+						.description("Game Deck Wiki Documentation")
+						.contact(new Contact().email("cnanfack@gmail.com").url("https://www.linkedin.com/in/charlynanfack/")))
+				;
 	}
 
 }
